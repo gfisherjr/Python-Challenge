d7f94a1de52839f5b7f7d3bba4a3211c44586243
gfisherjr <60365739+gfisherjr@users.noreply.github.com>
2020-02-29 13:16:24 -0500

    Create PyBank.py

---
+++ PyBank.py
@@ -0,0 +1,62 @@
+# PyBank Homework
+
+import os
+import csv
+import statistics
+
+#lists storing data
+dates = []
+profit_loss = []
+monthly_change = []
+
+new_pybank_list = zip(dates, profit_loss, monthly_change)
+new_pybank_list2 = zip(dates, profit_loss, monthly_change)
+
+# Directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+csvpath = os.path.join(dir_path, '..', 'PyBank_budget_data.csv')
+output_file = os.path.join(dir_path, 'outputfile.txt')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader) #print(f"CSV Header: {csv_header}")
+  
+    for row in csvreader:    
+        dates.append(row[0])
+        profit_loss.append(int(row[1]))
+
+
+#Calculate Monthly Change
+for i in range(len(profit_loss) - 1):
+    change = (profit_loss[i + 1] - profit_loss[i])
+    monthly_change.append(change)
+
+monthly_change.insert(0, 0) #insert value of zero in first element of monthly_change list
+
+#Calculate Average Monthly Change
+average_monthly_change = round(statistics.mean(monthly_change[1:]), 2)
+
+#Greatest Increase and Decrease in Profits:
+max_tuple = max((new_pybank_list), key=lambda x: x[2])
+min_tuple = min((new_pybank_list2), key=lambda y: y[2])
+
+print('\nFinancial Analysis')
+print('--------------------------')
+print(f'Total Months: {len(dates)}')
+print(f'Total: ${sum(profit_loss)}')
+print(f'Average Change: ${average_monthly_change}')
+print(f'Greatest Increase in Profits: {max_tuple[0]} (${max_tuple[2]})')
+print(f'Greatest Decrease in Profits: {min_tuple[0]} (${min_tuple[2]})')
+print()
+
+with open(output_file, 'w') as outfile:
+    outfile.write('Financial Analysis'
+                '\n--------------------------'
+                f'\nTotal Months: {len(dates)}'
+                f'\nTotal: ${sum(profit_loss)}'
+                f'\nAverage Change: ${average_monthly_change}'
+                f'\nGreatest Increase in Profits: {max_tuple[0]} (${max_tuple[2]})'
+                f'\nGreatest Decrease in Profits: {min_tuple[0]} (${min_tuple[2]})'
+    )
+    outfile.close()
